08765c92ee0f44b05f9a425df2edea6696d05145
FacundoOs <osores.facundo@gmail.com>
2020-11-11 13:55:09 +0100

    Create test for user can comment on an article

---
+++ spec/requests/api/comments_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe "POST /api/comments", type: require do
+  let!(:user) { create(:user) }
+  let!(:credentials) { user.create_new_auth_token }
+  let!(:headers) { { HTTP_ACCEPT: "application/json" }.merge!(credentials) }
+  let!(:article) { create(:article) }
+
+  describe "POST /api/comments" do
+    before do
+      post "/api/comments",
+           params: {
+             comment: {
+               article_id: article.id,
+               body: "This is a good article"
+             }
+           },
+           headers: headers
+    end
+
+    it "returns a 201 response" do
+      expect(response).to have_http_status 201
+    end
+
+    it "returns a sucess message" do
+      expect(response_json["message"]).to eq "succesfully saved"
+    end
+  end
+end
