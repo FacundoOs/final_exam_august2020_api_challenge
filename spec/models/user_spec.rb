e4fafde11625a02b8a2f709ea88affef8fa39e17
FacundoOs <osores.facundo@gmail.com>
2020-11-11 13:32:28 +0100

    Config the user factory bot. Add unit test for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "User has db columns" do
+    it { is_expected.to have_db_column :encrypted_password }
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :tokens }
+  end
+
+  describe "Validations" do
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :password }
+  end
+
+  describe "Factory" do
+    it "Should have valid factory" do
+      expect(create(:user)).to be_valid
+    end
+  end
+end
